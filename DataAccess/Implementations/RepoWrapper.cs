99ef6e9576a159c1bd8bf9bf5f7cc6aac793e01a
antitank19 <trankhaiminhkhoi10a3@gmail.com>
2022-06-03 10:41:34 +0700

    Add Repo Wrapper

---
+++ DataAccess/Implementations/RepoWrapper.cs
@@ -0,0 +1,25 @@
+﻿using OnlyFundsAPI.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyFundsAPI.DataAccess.Implementations
+{
+    class RepoWrapper
+    {
+        private IUserRepository _user;
+        public IUserRepository Users
+        {
+            get
+            {
+                if (_user == null)
+                {
+                    _user = new UserRepository();
+                }
+                return _user;
+            }
+        }
+    }
+}
