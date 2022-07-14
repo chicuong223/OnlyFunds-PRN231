06ddc9a895be5c70d1cc07577931a15ef2aae1ba
Chi Cuong Tang <chicuong.tang01@gmail.com>
2022-07-14 21:42:56 +0700

    Add EntitySet<Post>

--- API/Startup.cs
+++ API/Startup.cs
@@ -130,6 +130,7 @@ namespace API
             builder.EntitySet<User>("Users");
             builder.EntitySet<Report>("Reports");
             builder.EntitySet<PostTagMap>("PostTagMaps");
+            builder.EntitySet<Post>("Posts");
             builder.EntityType<Follow>().HasKey(follow => new { follow.FollowerID, follow.FolloweeID });
             builder.EntityType<Bookmark>().HasKey(bookmark => new { bookmark.UserID, bookmark.PostID });
             builder.EntityType<PostLike>().HasKey(pLike => new { pLike.UserID, pLike.PostID });
